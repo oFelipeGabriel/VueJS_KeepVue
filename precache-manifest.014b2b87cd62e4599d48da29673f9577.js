350745688db92affeb686cd4e21fc119c04de79e
Felipe Gabriel <felipegabriel@rocketmail.com>
2020-01-30 10:01:09 -0200

    add to gh-pages

---
+++ precache-manifest.014b2b87cd62e4599d48da29673f9577.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e176ca6912afd3daa2d",
+    "url": "/css/app.56e32f7c.css"
+  },
+  {
+    "revision": "d64a5fec54bbe1026f69",
+    "url": "/css/chunk-vendors.3387d42c.css"
+  },
+  {
+    "revision": "dcaf23fa6f04bdda2464dcdb97e8622c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e176ca6912afd3daa2d",
+    "url": "/js/app.00e22276.js"
+  },
+  {
+    "revision": "d64a5fec54bbe1026f69",
+    "url": "/js/chunk-vendors.a7b40793.js"
+  },
+  {
+    "revision": "d80751e9180127001c6a50d9e5ff0360",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
